2e387e7f20432f28acdc4b44a3675654135db5b8
Игорь <xx-prizrak-xx@yandex.ru>
2024-09-17 16:55:52 +0300

    6th project

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -6,19 +6,73 @@ import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    Set<Task> history = new LinkedHashSet<>();
+    private Map<Integer, Node> nodes = new HashMap<>();
+    private CustomLinkedList<Task> history = new CustomLinkedList<>();
+
+    class CustomLinkedList<T> {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public void addLast(T element) {
+            Node<T> oldTail = tail;
+            Node<T> newNode = new Node<>(tail, element, null);
+            tail = newNode;
+
+            if (oldTail != null) {
+                oldTail.next = newNode;
+            } else {
+                head = newNode;
+            }
+        }
+
+        public List<T> getTasks() {
+            Node<T> curHead = head;
+            List<T> tasks = new ArrayList<>();
+            while (curHead != null) {
+                tasks.add(curHead.data);
+                curHead = curHead.next;
+            }
+            return tasks;
+        }
+    }
 
     @Override
     public void historyAdd(Task task) {
-        if (history.contains(task)) {
-            history.remove(task);
+        if (task == null) {
+            return;
         }
-        history.add(task);
+        if (nodes.containsKey(task.getId())) {
+            removeNode(nodes.get(task.getId()));
+            nodes.remove(task.getId());
+        }
+        if (!nodes.isEmpty()) {
+            int tailID = history.tail.data.getId();
+            history.addLast(task);
+            nodes.put(task.getId(), history.tail);
+            nodes.replace(tailID, history.tail.prev);
+        } else {
+            history.addLast(task);
+            nodes.put(task.getId(), history.tail);
+        }
+
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return history.getTasks();
     }
 
+    public void removeNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        if (history.head.equals(node)) {
+            history.head = history.head.next;
+            history.head.prev = null;
+        } else if (history.tail.equals(node)) {
+            history.tail = history.tail.prev;
+        } else {
+            prevNode.next = node.next;
+            nextNode.prev = node.prev;
+        }
+    }
 }
--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -8,37 +8,46 @@ import static service.Managers.getDefaultHistory;
 class InMemoryHistoryManagerTest {
 
     @Test
-    void addTasks() {
+    void addTask() {
         HistoryManager historyManager = getDefaultHistory();
         Task task = new Task("name1", "desc1");
+        task.setId(1);
         historyManager.historyAdd(task);
         Task oldTask = task;
-        task = new Task("name11", "desc11");
-        historyManager.historyAdd(task);
         final List<Task> history = historyManager.getHistory();
         assertEquals(oldTask, history.get(0));
     }
 
     @Test
-    void addTask() {
+    void addTasks() {
         HistoryManager historyManager = getDefaultHistory();
-        Task task = new Task("name1", "desc1");
-        historyManager.historyAdd(task);
-        final List<Task> history = historyManager.getHistory();
+        Task task1 = new Task("name1", "desc1");
+        task1.setId(1);
+        Task task2 = new Task("name2", "desc2");
+        task2.setId(2);
+        Task task3 = new Task("name3", "desc3");
+        task3.setId(3);
+        historyManager.historyAdd(task1);
+        historyManager.historyAdd(task2);
+        historyManager.historyAdd(task3);
+        List<Task> history = historyManager.getHistory();
         assertNotNull(history);
-        assertEquals(1, history.size());
+        assertEquals(3, history.size());
     }
 
     @Test
     void addSameTask() {
         HistoryManager historyManager = getDefaultHistory();
         Task task1 = new Task("name1", "desc1");
+        task1.setId(1);
         Task task2 = new Task("name2", "desc2");
+        task2.setId(2);
         historyManager.historyAdd(task1);
         historyManager.historyAdd(task2);
         historyManager.historyAdd(task1);
         final List<Task> history = historyManager.getHistory();
         assertEquals(2, history.size());
         assertEquals(task2, history.get(0));
+        assertEquals(task1, history.get(1));
     }
 }
